03038b2fcd558c76d05f6a80b000975bef8a04a2
7heaven <caifangmao8@gmail.com>
2017-02-27 16:17:26 +0800

    make spinner test able to change loading string based on sys.argv

--- spinner_test.py
+++ spinner_test.py
@@ -5,5 +5,8 @@ from spinner_runner import SpinnerRunner
 import sys
 
 spinnerRunner = SpinnerRunner(sys.argv[1], True)
-spinnerRunner.setLoadingString('Fucking')
+
+if len(sys.argv) > 2:
+	spinnerRunner.setLoadingString(sys.argv[2])
+
 spinnerRunner.start()
